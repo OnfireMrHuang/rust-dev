feea0b598779b4360dde2ad806ecd1125b9158da
huangww01 <huangww01@mingyuanyun.com>
2022-04-29 10:31:00 +0800

    验证完成storage trait

--- kv/src/storage/mod.rs
+++ kv/src/storage/mod.rs
@@ -1,5 +1,9 @@
 
+mod memory;
+
 use crate::{KvError, Kvpair, Value};
+use memory::MemTable;
+
 
 /// 对存储的抽象，我们不关心数据存在哪儿，但需要定义外界如何和存储打交道
 pub trait Storage {
@@ -16,3 +20,88 @@ pub trait Storage {
     /// 遍历 HashTable，返回 kv pair 的 Iterator
     fn get_iter(&self, table: &str) -> Result<Box<dyn Iterator<Item = Kvpair>>, KvError>;
 }
+
+
+// 开始写测试用例
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+
+    #[test]
+    fn memtable_basic_interface_should_work() {
+        let store = MemTable::new();
+        test_basi_interface(store);
+    }
+    
+    // #[test]
+    // fn memetable_get_all_should_work() {
+    //     let store = MemTable::new();
+    //     test_get_all(store);
+    // }
+
+    fn test_basi_interface(store: impl Storage) {
+        // 第一次set会创建talbe，插入key并返回None(之前没值)   
+        let v = store.set("t1", "hello".into(), "world".into());
+        assert!(v.unwrap().is_none());
+
+        // 再次set同样的key会更新，并返回之前的值
+        let v1 = store.set("t1", "hello".into(), "world1".into()); 
+        assert_eq!(v1, Ok(Some("world".into())));
+
+        // get存在的key会得到最新的值
+        let v = store.get("t1", "hello");
+        assert_eq!(v, Ok(Some("world1".into())));
+        
+        // get 不存在的 key 或者 table 会得到 None 
+        assert_eq!(Ok(None), store.get("t1", "hello1")); 
+        assert!(store.get("t2", "hello1").unwrap().is_none());
+
+        // contains 纯在的 key 返回 true，否则 false 
+        assert_eq!(store.contains("t1", "hello"), Ok(true)); 
+        assert_eq!(store.contains("t1", "hello1"), Ok(false)); 
+        assert_eq!(store.contains("t2", "hello"), Ok(false));
+
+        // del 存在的 key 返回之前的值 
+        let v = store.del("t1", "hello"); 
+        assert_eq!(v, Ok(Some("world1".into())));
+
+        // del 不存在的 key 或 table 返回 None 
+        assert_eq!(Ok(None), store.del("t1", "hello1")); 
+        assert_eq!(Ok(None), store.del("t2", "hello"));
+    }
+
+    fn test_get_all(store: impl Storage) {
+        store.set("t2", "k1".into(), "v1".into()).unwrap();
+        store.set("t2", "k2".into(), "v2".into()).unwrap();
+        let mut data = store.get_all("t2").unwrap();
+        data.sort_by(|a,b|a.partial_cmp(b).unwrap());
+
+        assert_eq!(
+            data, 
+            vec![
+                Kvpair::new("k1", "v1".into()),
+                Kvpair::new("k2", "v2".into())
+            ]
+        );
+    }
+
+    fn test_get_iter(store: impl Storage) {
+        store.set("t2", "k1".into(), "v1".into()).unwrap();
+        store.set("t2", "k2".into(), "v2".into()).unwrap();
+
+        let mut data: Vec<_> = store.get_iter("t1").unwrap().collect();
+        data.sort_by(|a,b|a.partial_cmp(b).unwrap());
+        assert_eq!(
+            data,
+            vec![
+                Kvpair::new("k1", "v1".into()),
+                Kvpair::new("k2", "v2".into())
+            ]
+        )
+
+    }    
+}
+
+
+
